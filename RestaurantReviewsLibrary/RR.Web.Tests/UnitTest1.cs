e7b04f3da04ebf6ebd76d36ded77e978c035b8c1
Billy Do <dob@utexas.edu>
2018-05-05 23:23:05 -0400

    starting webpage project

---
+++ RestaurantReviewsLibrary/RR.Web.Tests/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace RR.Web.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
