3ca539a0c3d997bebcda4d32670004bd96e5f2e3
Billy Do <dob@utexas.edu>
2018-05-06 01:02:42 -0400

    added new RR.Web project

---
+++ RestaurantReviewsLibrary/RR.Web/Controllers/RestaurantsController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RR.Web.Controllers
+{
+    public class RestaurantsController : Controller
+    {
+        // GET: Restaurants
+        public ActionResult Index()
+        {
+            /* Top 3 restaurants shown, perhaps in carousel
+             * Each restaurant pic is a link to Details of restaurant
+             * Input to search for restaurant, or choose by Id --> Details/???
+             */
+            return View();
+        }
+
+        // GET: Restaurants/Details/5
+        public ActionResult Details(int id)
+        {
+            /* Show Details for restaurant
+             * Have "submit review" link, that goes to Reviews/Create
+             */
+            return View();
+        }
+
+        // GET: Restaurants/Create
+        public ActionResult Create()
+        {
+            /* Show Input form for Restaurant
+             **/
+            return View();
+        }
+
+        // POST: Restaurants/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Restaurants/Edit/5
+        public ActionResult Edit(int id)
+        {
+            /* Show input fields
+             * Each field should have current info as value
+             */
+            return View();
+        }
+
+        // POST: Restaurants/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Restaurants/Delete/5
+        public ActionResult Delete(int id)
+        {
+            /* Ask if they want to delete
+             * Show restaurant info
+             */
+            return View();
+        }
+
+        // POST: Restaurants/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ RestaurantReviewsLibrary/RR.Web/Controllers/ReviewsController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace RR.Web.Controllers
+{
+    public class ReviewsController : Controller
+    {
+        // GET: Reviews
+        public ActionResult Index()
+        {
+            /* Show 3 random reviews of restaurants?
+             * Same as Restaurants, have "search for" stuff at bottom
+             */
+            return View();
+        }
+
+        // GET: Reviews/Details/5
+        public ActionResult Details(int id)
+        {
+            var review = new DataAccessLayer.Models.Review
+            {
+                Id = 1,
+                Rating = 4,
+                RestaurantId = 5,
+                Username = "googleuser",
+                Description = "I give it a 4/100."
+            };
+
+            return View(review);
+        }
+
+        // GET: Reviews/Create
+        public ActionResult Create()
+        {
+            /* Show fields for creating a review
+             */
+            return View();
+        }
+
+        // POST: Reviews/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reviews/Edit/5
+        public ActionResult Edit(int id)
+        {
+            /* show fields allowed for editing a review
+             * 
+             */
+            return View();
+        }
+
+        // POST: Reviews/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Reviews/Delete/5
+        public ActionResult Delete(int id)
+        {
+            /* Ask if they want to delete review
+             * Show review Details
+             */
+            return View();
+        }
+
+        // POST: Reviews/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
